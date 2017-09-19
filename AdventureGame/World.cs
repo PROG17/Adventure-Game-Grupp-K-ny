b0a7837df32442c5c6014db3c1fa8cb17160e685
Karl Fristedt <karl.fristedt@yh.nackademin.se>
2017-09-19 21:27:45 +0200

    Har lagt till klassen "world" där rummen skapas och innehåller en metod för att spela.

---
+++ AdventureGame/World.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    class World
+    {
+        // Skapa spelare från klassen "Player". t.ex. Player kalle = new Player("Kalle", osv);
+
+        // Generera värld bestånde av ett antal rum
+        // T.ex.
+        // Room hall = new Room() { RoomName = "Hall", description = "Det är hallen" };
+        // Room köket = new Room() { RoomName = "Köket", description = "Det är köket" };
+        // Room toaletten = new Room() { RoomName = "Toaletten", description = "Det här toaletten"};
+
+        // Lägg till utgångar och vart dom leder
+        // T.ex.
+        // hall.exits.Add(Direction.South, köket);
+        // köket.exits.Add(Direction.East, toaletten);
+
+        // sedan kommer det behöva mer kod för att lägga till t.ex. items till respektive rum
+
+        /**************************************************************************/
+
+        // public void Play () // Metod för att spela
+
+        // Start "gameloop". En while loop t.ex.
+
+        // 1. Använd en rumspekare typ currentroom = t.ex. Hall som skapades tidigare
+        // 2. Skriv ut kommandoprompt typ >
+        // 3. Läs in kommando från spelaren med string command = Console.ReadLine();
+        // 4. Kod för att tolka kommandot och utföra. t.ex "go west", "take key" etc.
+        // 5. samt kontrollera att det är ett alternativ man kan göra i det rummet man är i.
+        // 6. annars ska man få ett felmeddelande och gå tillbaka till början av loopen.
+        // 7. 
+
+
+
+
+
+    }
+}
