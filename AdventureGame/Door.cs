4d77e102991278383712189edc106ee896a881cd
Elias Mohammed <eliasrmohammed@hotmail.com>
2017-09-19 16:25:29 +0200

    Lagt till klasser för Door och Items

---
+++ AdventureGame/Door.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    class Door
+    {
+        public string DoorName { get; set; }
+        public bool DoorIsClosed { get; set; }
+
+        //Här finns dörrar mellan rummen.
+        //Vissa dörrar är öppna. 
+        //Vissa dörrar behöver nycklar från klass item för att öppns.
+
+
+    }
+}
